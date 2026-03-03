b87ce13a2c583b142691e8468df010b3218cd367
My laptop <samiraomar.xx@gmail.com>
2026-03-03 22:51:15 +0300

    updated index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 
 <head>
   <meta charset="UTF-8" />
+  <meta name="google-site-verification" content="uKtr2UsvXYFJ7ODbXEfgpqLQWKyUk05eIaa_YJjI_XU" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta name="description"
     content="Get a modern website for your service-based business with booking and client forms — make booking easy and grow your business fast." />
