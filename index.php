c59f46636836c8170c3b74e5a945347e83998703
My laptop <samiraomar.xx@gmail.com>
2026-03-06 23:21:19 +0300

    updated index.php

--- index.php
+++ index.php
@@ -3,15 +3,17 @@
 
 <head>
   <meta charset="UTF-8" />
-  <meta name="google-site-verification" content="uKtr2UsvXYFJ7ODbXEfgpqLQWKyUk05eIaa_YJjI_XU"/>
+  <meta name="google-site-verification" content="uKtr2UsvXYFJ7ODbXEfgpqLQWKyUk05eIaa_YJjI_XU" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <meta name="description"
-    content="Get a modern website for your service-based business with booking and client forms — make booking easy and grow your business fast." />
+
 
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
 
   <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+
   <title>Websites with Built-In Booking & Lead Capture</title>
+  <meta name="description"
+    content="Get a modern website for your service-based business with booking and client forms — make booking easy and grow your business fast." />
 
   <!-------------------------manifest------>
   <!-- Favicon for browser tabs -->
